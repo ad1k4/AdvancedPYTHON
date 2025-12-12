52d4e80f7a41a222674a78fdff00c4e203862774
ad1k4 <241337@astanait.edu.kz>
2025-12-12 19:13:15 +0500

    6task

---
+++ 1 week/6task.py
@@ -0,0 +1,11 @@
+a=int(input("First number: "))
+b=input("Operations: ")
+c=int(input("Second numer: "))
+if (b=='+'):
+    print(a+c)
+elif (b=='-'):
+    print(a-c)
+elif (b=='*'):
+    print(a*c)
+else:
+    print(int(a/c)) 
