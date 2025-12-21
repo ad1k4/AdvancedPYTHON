693bae32e950c806f8c69dc117ab75c1cb4b22cb
ad1k4 <241337@astanait.edu.kz>
2025-12-21 10:51:27 +0500

    first task

---
+++ 2 week/1 task.py
@@ -0,0 +1,9 @@
+text = input().strip()
+count = 0
+for i in range(len(text) - 4):
+    part=text[i:i+5]
+    if part=="<--<<":
+        count+=1
+    if part==">>-->":
+        count+=1
+print(count)
